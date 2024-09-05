11ec726256f48a565899dee2c69394efa6abb9d8
Karol <karolkrawczyk8650@gmail.com>
2024-09-06 00:40:45 +0200

    refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // Import FormsModule here
+import { Routes, RouterModule } from '@angular/router';
 
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './components/main/main.component';
+import { SectionComponent } from './components/section/section.component';
+
+const routes: Routes = [
+  { path: '', component: MainComponent }, // Main component displayed at "/"
+  { path: 'section/:id', component: SectionComponent }, // Section component displayed at "/section/:id"
+  { path: '**', redirectTo: '' } // Redirect unknown routes to the main component
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule 
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
