51e52f84e74cf763e6c047f5b63d3494d82708cb
Karol <karolkrawczyk8650@gmail.com>
2024-09-08 18:46:45 +0200

    section-area

---
+++ src/app/components/section-area/section-area.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SectionAreaComponent } from './section-area.component';
+
+describe('SectionAreaComponent', () => {
+  let component: SectionAreaComponent;
+  let fixture: ComponentFixture<SectionAreaComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SectionAreaComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SectionAreaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/section-area/section-area.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { Area } from '../../models/area.model';
+
+@Component({
+  selector: 'app-section',
+  templateUrl: './section-area.component.html',
+  styleUrls: ['./section-area.component.css']
+})
+export class SectionAreaComponent implements OnInit {
+  activeArea = false;
+  sectionId!: number;
+  areas: Area[] = [
+    { id: 1, name: "Streak" },
+    { id: 2, name: "Reflections" },
+    { id: 3, name: "Ideas" },
+    { id: 4, name: "Goals" },
+    { id: 5, name: "To Do" },
+    { id: 6, name: "Planner" }
+  ];
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.sectionId = +params.get('id')!;
+    });
+  }
+
+  viewArea(areaName: string): void {
+    this.router.navigate([areaName.toLowerCase()], { relativeTo: this.route });
+  }
+}
