f6b508fbb0fc0a45b5ff531734cce3dc13a88321
Karol <karolkrawczyk8650@gmail.com>
2024-09-08 18:47:15 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { AppComponent } from './app.component';
 import { SectionComponent } from './components/section/section.component';
 import { SectionAreaComponent } from './components/section-area/section-area.component';
 import { StreakComponent } from './components/streak/streak.component';
+import { ReflectionComponent } from './components/reflections/reflection.component';
+
 
 const routes: Routes = [
   { path: '', component: SectionComponent },
   { path: 'section/:name', component: SectionAreaComponent },
   { path: 'section/:name/streak', component: StreakComponent },
-  { path: '**', redirectTo: '' }
+  { path: 'section/:name/reflections', component: ReflectionComponent },
+  { path: 'section', component: SectionComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
@@ -21,7 +25,8 @@ const routes: Routes = [
     AppComponent,
     SectionComponent,
     SectionAreaComponent,
-    StreakComponent
+    StreakComponent,
+    ReflectionComponent
   ],
   imports: [
     BrowserModule,
