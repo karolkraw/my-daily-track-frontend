f2e316ac2abdbff408f7f90cd44751247b3b9177
Karol <karolkrawczyk8650@gmail.com>
2024-09-30 15:11:36 +0200

    clean up

--- src/app/components/task-manager/task-manager.component.ts
+++ src/app/components/task-manager/task-manager.component.ts
@@ -96,12 +96,6 @@ export class TaskManagerComponent implements OnInit {
       );
   }
 
-  /* onDateChange(event: any): void {
-    this.newDeadline = event.value ? event.value : null;
-    console.log("onDateChange " + this.newDeadline)
-    //this.searchReflection();
-  } */
-
   switchPanel(panel: string) {
     this.activePanel = panel;
   }
@@ -151,62 +145,31 @@ export class TaskManagerComponent implements OnInit {
         showAddSubtaskForm: false,
       }
     
-      
       this.taskManagerService.createTask(task, this.sectionName).subscribe(
-        (data: any) => {
-          console.log("returned task")
-          this.currentTasks.push(task)
-         
+        (data: any) => {       
+          this.currentTasks.push(task) 
         },
         error => {
           console.error('Error fetching tasks data', error);
         }
       );
-     /*  this.tasks.push({
-        title: this.newTaskTitle,
-        subtasks: [],
-        completed: false,
-        isEditing: false,
-        newSubtaskTitle: '',
-      }); */
-      this.newTaskTitle = '';  // Clear the input after adding the task
+      this.newTaskTitle = '';
     }
   }
 
   addSubtask(task: any) {
     if (this.newSubtask.title && this.newSubtask.description && this.newSubtask.deadline) {
-      const createdDate = this.formatDate(new Date()); // Set the current date
-      const subtask = { ...this.newSubtask, createdDate };
-      this.newSubtask = { title: '', description: '', deadline: '', completed: false }; // Reset form
-      task.showAddSubtaskForm = false;  // Hide form
+      const createdDate = this.formatDate(new Date());
+      const subtask = { ...this.newSubtask, createdDate: createdDate, deadline: this.newSubtask.deadline };
+      this.newSubtask = { title: '', description: '', deadline: '', completed: false };
+      task.showAddSubtaskForm = false;
       this.taskManagerService.addSubtaskToTask(task.title, subtask, this.sectionName).subscribe(data => {
         task.subtasks.push(data);
-
-        console.log(data)
-    },
-
-    )
+      })
     } else {
       alert('Please fill in all fields.');
     }
   }
-
-  // Add a new subtask to a specific task
-  /* addSubtaskToTask(taskIndex: number) {
-     const task = this.currentTasks[taskIndex];
-    if (this.newSubtaskTitle.trim()) {
-      const subtask: Subtask =  { title: this.newSubtaskTitle, description: "asdmksd", createdDate: this.formatDate(new Date()),
-         deadline: this.formatDate(new Date()), completed: false }
-      this.taskManagerService.addSubtaskToTask(task.title, subtask, this.sectionName).subscribe(data => {
-          task.subtasks.push(data);
-
-          console.log(data)
-      },
-
-      )
-      this.newSubtaskTitle = ''; // Reset the subtask input for this task
-    } 
-  } */
 
   completeSubtask(taskIndex: number, subtaskIndex: number) {
     const task = this.currentTasks[taskIndex]
@@ -219,7 +182,6 @@ export class TaskManagerComponent implements OnInit {
     )
   }
 
-
   removeSubtask(taskIndex: number, subtaskIndex: number) {
     const task = this.currentTasks[taskIndex]
     const subtask = task.subtasks[subtaskIndex]
@@ -230,19 +192,14 @@ export class TaskManagerComponent implements OnInit {
     )
   }
 
-  // Edit a task (show the input field)
   editTask(index: number) {
-    //this.tasks[index].isEditing = true;
     this.isEditing = true;
   }
 
-  // Save the edited task title
   updateTask(index: number) {
-    // this.tasks[index].isEditing = false;
     this.isEditing = false;
   }
 
-  // Delete a task
   deleteTask(index: number) {
     this.currentTasks.splice(index, 1);
   }
